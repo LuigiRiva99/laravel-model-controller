cd8b962d6299403a3b0b265de0dd879ce101f65f
LuigiRiva99 <luigi.riva.1999@gmail.com>
2024-05-23 16:18:08 +0200

    ADD: Movie model, PageController controller, movie cards from database data

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,25 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Home</h1>
+    <h1>Movies</h1>
+
+    <section class="card-section">
+        <div class="container">
+            <div class="row row-gap-4">
+                @foreach ($movies as $movie)
+                    <div class="col-3">
+                        <div class="card ms_card">
+                            <div class="card-body text-center">
+                                <p class="title">{{ $movie->title }}</p>
+                                <p>{{ $movie->original_title }}</p>
+                                <p>{{ $movie->nationality }}</p>
+                                <p>{{ $movie->date }}</p>
+                                <p>Voto: {{ $movie->vote }}</p>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+        </div>
+    </section>
 @endsection
